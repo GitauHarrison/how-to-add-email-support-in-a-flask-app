02dceb047e650981d1ab1b9b41046266a52cfd3e
harry <harrisongitau3@gmail.com>
2021-03-29 15:20:03 +0300

    Complete password reset

---
+++ .flaskenv
@@ -0,0 +1,5 @@
+
+FLASK_APP=sendgrid.py
+FLASK_ENV=development
+FLASK_DEBUG=True
+START_NGROK=1
---
+++ app/__init__.py
@@ -0,0 +1,64 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_mail import Mail
+from config import Config
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+bootstrap = Bootstrap(app)
+login = LoginManager(app)
+login.login_view = 'login'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+mail = Mail(app)
+
+
+
+
+if not app.debug and not app.testing:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'],
+                    app.config['MAIL_PASSWORD']
+                    )
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='noreply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['MAIL_DEFAULT_SENDER'],
+            subject='Flask Email Support Failure',
+            credentials=auth, secure=secure
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+    if app.config['LOG_TO_STDOUT']:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        app.logger.addHandler(stream_handler)
+    else:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler(
+            'logs/flask_emial_support.log',
+            maxBytes=10240,
+            backupCount=10
+        )
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+        ))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Flask Email Support')
+
+from app import routes, models, errors
